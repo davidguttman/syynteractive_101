63a5dc53876f2895559d3ba44d10270e4d0ab0c5
David Guttman <david@davidguttman.com>
2012-01-29 12:40:12 -0800

    processing setup up

--- project/js/main.js
+++ project/js/main.js
@@ -1,1 +1,21 @@
 console.log("Welcome!");
+
+var startProcessing = function(p5) {
+  
+  p5.setup = function() {
+    
+  };
+  
+  p5.draw = function() {
+    
+  };
+  
+};
+
+$(document).ready(function() {
+
+  var canvas = document.getElementById('p5');
+  
+  new Processing(canvas, startProcessing);
+
+});
