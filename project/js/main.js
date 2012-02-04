3e5fa9d0825f2bcf68e11fcf22622a02c1ec7470
David Guttman <david@davidguttman.com>
2012-02-04 08:53:49 -0800

    Added comments

--- project/js/main.js
+++ project/js/main.js
@@ -1,21 +1,47 @@
 console.log("Welcome to Interactive Visuals 101!");
 
+
+////////////////
+// PAGE SETUP //
+////////////////
+
+// This grabs the document and tells the page what to do when it's ready
+$(document).ready(function() {
+
+  // Select the canvas element
+  var canvas = document.getElementById('p5');
+  
+  // Create a new Processing instance with 'canvas' and pass it to 'startProcessing'
+  new Processing(canvas, startProcessing);
+
+});
+
+
+///////////////
+// Fun Stuff //
+///////////////
+
+// This function will receive the Processing instance created above, and will refer to it internally as 'p5'
 var startProcessing = function(p5) {
   
+  // This will be called once at the beginning
   p5.setup = function() {
-    
+        
   };
   
+  // This will be called every frame
   p5.draw = function() {
+    
+  };
+
+  // This will be called every time the mouse moves
+  p5.mouseMoved = function() {
+    
+  };
+
+  // This will be called every time the mouse is clicked
+  p5.mouseClicked = function() {
     
   };
   
 };
-
-$(document).ready(function() {
-
-  var canvas = document.getElementById('p5');
-  
-  new Processing(canvas, startProcessing);
-
-});
